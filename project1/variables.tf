75b7053e642e9caa8dc8f6cf9f621a9f41518bd6
promise <frankpromiseedah@gmail.com>
2022-07-26 15:45:10 +0200

    variables

---
+++ project1/variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "key_name" {}
+variable "region" {
+
+  default = "us-east-1"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+
+}
+
+variable "subnet_prefix" {}
